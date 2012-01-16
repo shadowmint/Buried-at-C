05ccc608f30feab80b4ba761b3036b8d3a08c095
Doug <linderd@iinet.net.au>
2012-01-16 21:23:47 +0800

    Initial import

---
+++ tutorials/testing/hello-world/build/CMakeFiles/test-runner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test-runner.dir/src/main.c.o"
+  "test-runner.pdb"
+  "test-runner"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/test-runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
